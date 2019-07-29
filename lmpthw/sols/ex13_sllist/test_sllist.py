d33e7690c415a42c48b56bd651a0ba5ecccc384d
kennyfrc <kennyfrc@Kenns-MacBook-Pro.local>
2019-07-29 22:25:36 +0800

    update test suite

--- lmpthw/sols/ex13_sllist/test_sllist.py
+++ lmpthw/sols/ex13_sllist/test_sllist.py
@@ -9,6 +9,8 @@ def test_push():
     colors.push("Ultramarine Violet")
     assert colors.count() == 3
 
+test_push()
+
 def test_pop():
     colors = SingleLinkedList()
     colors.push("Magenta")
@@ -16,3 +18,90 @@ def test_pop():
     assert colors.pop() == "Alizarin"
     assert colors.pop() == "Magenta"
     assert colors.pop() == None
+
+test_pop()
+
+def test_unshift():
+    colors = SingleLinkedList()
+    colors.push("Viridian")
+    colors.push("Sap Green")
+    colors.push("Van Dyke")
+    assert colors.unshift() == "Viridian"
+    assert colors.unshift() == "Sap Green"
+    assert colors.unshift() == "Van Dyke"
+    assert colors.unshift() == None
+
+test_unshift()
+
+def test_shift():
+    colors = SingleLinkedList()
+    colors.shift("Cadmium Orange")
+    assert colors.count() == 1
+
+    colors.shift("Carbazole Violet")
+    assert colors.count() == 2
+    assert colors.pop() == "Cadmium Orange"
+    assert colors.count() == 1
+    assert colors.pop() == "Carbazole"
+
+test_shift()
+
+def test_remove():
+    colors = SingleLinkedList()
+    colors.push("Cobalt")
+    colors.push("Zinc White")
+    colors.push("Nickle Yellow")
+    colors.push("Perinone")
+    assert colors.remove("Cobalt") == 0
+    colors.dump("before perinone")
+    assert colors.remove("Perinone") == 2
+    colors.dump("after perinone")
+    assert colors.remove("Nickle Yellow") == 1
+    assert colors.remove("Zinc White") == 0
+
+test_remove()
+
+def test_first():
+    colors = SingleLinkedList ()
+    colors.push("Cadmium Red Light")
+    assert colors.first() == "Cadmium Red Light"
+    colors.push("Hansa Yellow")
+    assert colors.first() == "Cadmium Red Light"
+    colors.shift("Pthalo Green")
+    assert colors.first() == "Pthalo Green"
+
+test_first()
+
+def test_last ():
+    colors = SingleLinkedList()
+    colors.push("Cadmium Red Light")
+    assert colors.last() == "Cadmium Red Light"
+    colors.push("Hansa Yellow")
+    assert colors.last() == "Hansa Yellow"
+    colors.shift("Pthalo Green")
+    assert colors.last() == "Hansa Yellow"
+
+test_last()
+
+def test_get():
+    colors = SingleLinkedList ()
+    colors.push("Vermillion")
+    assert colors.get(0) == "Vermillion"
+    colors.push("Sap Green")
+    assert colors.get(0) == "Vermillion"
+    assert colors.get(1) == "Sap Green"
+    colors.push("Cadmium Yellow Light")
+    assert colors.get(0) == "Vermillion"
+    assert colors.get(1) == "Sap Green"
+    assert colors.get(2) == "Cadmium Yellow Light"
+    assert colors.pop() == "Cadmium Yellow Light"
+    assert colors.get(0) == "Vermillion"
+    assert colors.get(1) == "Sap Green"
+    assert colors.get(2) == None
+    colors.pop ()
+    assert colors.get(0) == "Vermillion"
+    colors.pop ()
+    assert colors.get(0) == None
+
+test_get()
+
